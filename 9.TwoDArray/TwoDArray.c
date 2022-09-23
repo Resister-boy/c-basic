c588bdba000724e76f0e78585b0ae39860f70660
Resister-boy <hassanpumped17@gmail.com>
2022-09-23 14:36:42 +0900

    TwoDArray

---
+++ 9.TwoDArray/TwoDArray.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  // int tarr[3][4] = {
+  //   {1, 2, 3, 4},
+	// 	{5, 6, 7, 8},
+	// 	{9, 10, 11, 12}
+  // }
+
+  // for(int i = 0; i < 3; i++) {
+  //   for(int j = 0; j < 4; j++) {
+  //     printf("%d", tarr[i][j]
+  //   }
+  // }
+
+  int tarr[3][4];
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 4; j++) {
+      tarr[i][j] = (i + 1)*(j + 1);
+      printf("%d", tarr[i][j]);
+      printf(" ");
+    }
+    printf("\n");
+  }
+}
